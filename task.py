b9003cf10de16968980aec5ddd7e5726b7457c4e
aathul4137 <aathul4137@gmail.com>
2024-07-13 10:30:06 +0530

    first commit

---
+++ task.py
@@ -0,0 +1,35 @@
+def hexagon_pattern(rows, cols):
+    for row in range(rows):
+        # Top part of the hexagons
+        for col in range(cols):
+            print("  ___  ", end=" ")
+        print()
+        
+        
+        for col in range(cols):
+            if col < 4:
+                if col == 0:
+                    print(" /   \___/", end=" ")
+                else:
+                    print("  \___/", end=" ")
+            else:
+                print("  \ ", end=" ")
+                    
+        print()
+    
+        
+        
+        
+        for col in range(cols):
+            if col % 2 == 0 :
+                print(" \___/ ", end=" ")
+            else:
+                print(" \___/ ", end=" ")
+            
+        print()
+        
+       
+
+rows = 3
+cols = 5
+hexagon_pattern(rows, cols)
